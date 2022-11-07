9e5bdc4ae9728d1d7c8b988e6f634e1cbd79b1dd
ItzAryan107 <aryangithub2017@gmail.com>
2022-11-07 10:14:29 +0530

    initial commit

--- src/LeetCode/ReverseInteger.java
+++ src/LeetCode/ReverseInteger.java
@@ -1,5 +1,6 @@
 package LeetCode;
 import java.util.Scanner;
+
 public class ReverseInteger {
     public boolean inRange(int num){
         return num >= Integer.MIN_VALUE / 10 && num <= Integer.MAX_VALUE / 10;
@@ -17,6 +18,18 @@ public class ReverseInteger {
         x = x /10;
         return reverse(x);
     }
+
+//    public int reverseRecursive(int num){
+//        if (num>=0 && num<=9){
+//            rev = rev * 10 + num;
+//            return  rev;
+//        }
+//
+//        int a = num%10;
+//        rev = rev*10+a;
+//        reverseRecursive(num/10);
+//        return rev;
+//    }
 
     public static void main(String[] args){
         ReverseInteger test = new ReverseInteger();
